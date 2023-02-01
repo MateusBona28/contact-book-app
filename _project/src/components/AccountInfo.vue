0110c85514563e702164b1f3c3091a773f24e20a
mateus <mateusbona28@gmail.com>
2023-02-01 19:41:11 -0300

    feat: base do componente de informações do usuário finalizado

---
+++ _project/src/components/AccountInfo.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'AccountInfo'
+}
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style>
+</style>
--- _project/src/components/Home.vue
+++ _project/src/components/Home.vue
@@ -1,14 +1,16 @@
 <script>
 import Header from './Header.vue';
+import AccountInfo from './AccountInfo.vue';
 
 export default {
   name: 'Home',
-  components: { Header }
+  components: { Header, AccountInfo }
 }
 </script>
 
 <template>
   <Header></Header>
+  <AccountInfo></AccountInfo>
 </template>
 
 <style>
